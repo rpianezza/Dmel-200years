516334d822c8b95da3b4f1c594a49825a3e8eede
Almo96 <almorscarpa@gmail.com>
2025-01-27 16:12:21 +0100

    Fix PopFreq

--- figures/supp_figures/Pop_freq/PopTE2_freq.R
+++ figures/supp_figures/Pop_freq/PopTE2_freq.R
@@ -6,8 +6,6 @@ df<-read.csv("/Volumes/Element/Backup/dmel_twocenturies_local/Revision/final_USA
 df2<-read.csv("/Volumes/Element/Backup/dmel_twocenturies_local/Revision/final_USA_UK/output_sh_3_UK/UK-min-count3.teinsertions_fixed", header=FALSE, sep = "\t")
 df3<-read.csv("/Volumes/Element/Backup/dmel_twocenturies_local/Revision/final_USA_UK/output_sh_3/USA-min-count3.teinsertions_fixed", header=FALSE, sep = "\t")
 
-
-
 colnames(df)<-c("ID", "chr", "postion", "strand", "TE_family", "order", "support", "comment", "TE_frequency")
 colnames(df2)<-c("ID", "chr", "postion", "strand", "TE_family", "order", "support", "comment", "TE_frequency")
 colnames(df3)<-c("ID", "chr", "postion", "strand", "TE_family", "order", "support", "comment", "TE_frequency")
@@ -30,9 +28,9 @@ df3 <- df3 %>%
     TE_family = str_replace(TE_family, "MLE_diverged", "McLE_diverged")
   )
 
-TE_order <- c("Blood","Opus" ,"412","Tirant","I-ele","Hobo" ,"P-ele","Spoink", "MLE","Souslik","Transib1",
+TE_order <- c("Blood","Opus" ,"412","Tirant","I-ele","Hobo" ,"P-ele","Spoink", "McLE","Souslik","Transib1",
               "Blood_diverged","Opus_diverged" ,"412_diverged","Tirant_diverged","I-ele_diverged","Hobo_diverged",
-              "Spoink_diverged", "MLE_diverged","Souslik_diverged","Transib1_diverged")
+              "Spoink_diverged", "McLE_diverged","Souslik_diverged","Transib1_diverged")
 
 df$TE_family <- factor(df$TE_family, levels = TE_order)
 df2$TE_family <- factor(df2$TE_family, levels = TE_order)
@@ -41,6 +39,22 @@ df3$TE_family <- factor(df3$TE_family, levels = TE_order)
 df <- df %>% mutate(TE_type = ifelse(grepl("diverged", TE_family, ignore.case = TRUE), "diverged", "canonical"))
 df2 <- df2 %>% mutate(TE_type = ifelse(grepl("diverged", TE_family, ignore.case = TRUE), "diverged", "canonical"))
 df3 <- df3 %>% mutate(TE_type = ifelse(grepl("diverged", TE_family, ignore.case = TRUE), "diverged", "canonical"))
+
+
+df <- df %>%
+  group_by(TE_family) %>%
+  filter(n() >= 3) %>%
+  ungroup()
+
+df2 <- df2 %>%
+  group_by(TE_family) %>%
+  filter(n() >= 3) %>%
+  ungroup()
+
+df3 <- df3 %>%
+  group_by(TE_family) %>%
+  filter(n() >= 3) %>%
+  ungroup()
 
 
 g1 <- ggplot(df, aes(x = TE_family, y = TE_frequency, fill = TE_type)) +
@@ -62,7 +76,7 @@ g2 <- ggplot(df2, aes(x = TE_family, y = TE_frequency, fill = TE_type)) +
   scale_fill_manual(values = c("diverged" = "#F8766D", "canonical" = "#619CFF"))+
   labs(fill = "Insertion type")
 
-g3 <- ggplot(df2, aes(x = TE_family, y = TE_frequency, fill = TE_type)) +
+g3 <- ggplot(df3, aes(x = TE_family, y = TE_frequency, fill = TE_type)) +
   geom_point() +
   geom_boxplot() +
   labs(x = "TE Family", y = "TE Frequency") +  
@@ -79,22 +93,3 @@ g <- ggarrange(
 )
 
 plot(g)
-
-#java -jar popte2-v1.10.03.jar se2pe --fastq1 trimmed/SRR3939083_1_clean.fastq.gz --fastq2 trimmed/SRR3939083_2_clean.fastq.gz --bam1 map/USA_2014_1.sam --bam2 map/USA_2014_2.sam --sort --output output/USA.sort.bam 
-#java -jar popte2-v1.10.03.jar ppileup --bam output/USA.sort.bam --map-qual 15 --hier ../../Supp_fig/PopTE2/names_TEs.hier --output output/USA.ppileup.gz
-#java -jar popte2-v1.10.03.jar identifySignatures --ppileup output/USA.ppileup.gz --mode separate --output output/USA3.signatures --min-count 3
-#java -jar popte2-v1.10.03.jar frequency --ppileup output/UK.ppileup.gz --signature output/UK3.signatures --output output/UK3.freqsig 
-#java -jar popte2-v1.10.03.jar filterSignatures --input output/USA3.freqsig --output output/USA3.filter.freqsig --max-otherte-count 2 --max-structvar-count 2
-#java -jar popte2-v1.10.03.jar pairupSignatures --signature output/USA3.filter.freqsig --ref-genome ../../Supp_fig/PopTE2/Dmel_TE_merged.fa --hier ../../Supp_fig/PopTE2/names_TEs.hier --min-distance -200 --max-distance 300 --output output/USA3.filter.teinsertions
-
-#Remove the commas the german java puts to separate the thousands: sed 's/,//g' output_final/USA-min-count3.freqsig > output_final/USA-min-count3_fixed.freqsig
-
-#java -jar popte2-v1.10.03.jar se2pe --fastq1 trimmed/SRR3939083_1_clean.fastq.gz --fastq2 trimmed/SRR3939083_2_clean.fastq.gz --bam1 map/USA_2014_1.sam --bam2 map/USA_2014_2.sam --sort --output output/USA.sort.bam 
-#java -jar popte2-v1.10.03.jar ppileup --bam output/USA.sort.bam --map-qual 15 --hier ../../Supp_fig/PopTE2/names_TEs.hier --output output/USA.ppileup.gz
-#java -jar popte2-v1.10.03.jar identifySignatures --ppileup output_final/USA.ppileup.gz --mode separate --output output_final/USA-min-count3.signatures --min-count 3
-#java -jar popte2-v1.10.03.jar frequency --ppileup output_final/USA.ppileup.gz --signature output_final/USA-min-count3.signatures --output output_final/USA-min-count3.freqsig
-#sed 's/,//g' output_final/USA-min-count3.freqsig > output_final/USA-min-count3_fixed.freqsig
-#java -jar popte2-v1.10.03.jar filterSignatures --input output_final/USA-min-count3_fixed.freqsig --output output_final/USA-min-count3.filter.freqsig  --max-otherte-count 2 --max-structvar-count 2
-#sed 's/,//g' output_final/USA-min-count3.filter.freqsig > output_final/USA-min-count3_fixed.filter.freqsig
-#java -jar popte2-v1.10.03.jar pairupSignatures --signature output_final/USA-min-count3_fixed.filter.freqsig --ref-genome ../../Supp_fig/PopTE2/Dmel_TE_merged.fa --hier names_TEs.hier --min-distance -200 --max-distance 300 --output output_final/USA-min-count-3.teinsertions
-#sed 's/\([0-9]\),\([0-9]\)/\1.\2/g' USA-min-count-3.teinsertions > USA-min-count-3.teinsertions_fixed
